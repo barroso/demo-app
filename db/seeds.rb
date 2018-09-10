b2de63ca16b9aa6f17cad9ad567c5c6f9b169896
Francisco Barroso <franciscobarrosoflf@gmail.com>
2018-09-10 20:44:41 -0300

    exemplo de dados para dar carga no banco

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+List.create(title:"Viagem Janeiro", excerpt:"Uma viagem legal")
+List.create(title:"Equipamentos de fotografia", excerpt:"Minha seleção de equipamentos para fotos externas")
